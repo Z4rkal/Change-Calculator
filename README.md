1f8b934432b026a8f0f02516e7437ee266f9db93
Michael Fischer <michael@Jonathans-MacBook-Pro.local>
2019-05-24 16:27:26 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple change calculator. It takes to numbers as input and outputs the correct amount of change to give a customer.
+
+To run this, use npm install and npm start.
--- main.js
+++ main.js
@@ -1,1 +1,75 @@
 // Write your JavaScript here
+function calculateChange(amtDue,amtGiven) {
+/*    //amtDue = parseInt(String(amtDue * 100));
+    //amtGiven = parseInt(String(amtGiven * 100));
+    changeDue = amtGiven - amtDue;//parseInt(String(amtGiven - amtDue));
+*/  var result = []; //[dollars,quarters,dimes,nickels,pennies]
+    changeDue = amtGiven - amtDue;
+    // console.log('Change due: ' + changeDue);
+    var [dol,dolC] = handleCalculation(changeDue,100);
+    // console.log(dol + ' ' + dolC);
+    var [qrt,qrtC] = handleCalculation(dolC,25);
+    var [dim,dimC] = handleCalculation(qrtC,10);
+    var [nic,nicC] = handleCalculation(dimC,5);
+    var [pen,] = handleCalculation(nicC,1);
+/*  console.log('Due: ' + amtDue + ' Given: ' + amtGiven + ' Total Change: ' + changeDue + '\n');
+    var [dol,dolC] = handleCalculation(changeDue,1);
+    console.log('Dollars ' + dol + '\n');
+    var [qrt,qrtC] = handleCalculation(dolC,.25);
+    console.log('Quarters: ' + qrt + '\n');
+    var [dim,dimC] = handleCalculation(qrtC,.1);
+    console.log('Dimes: ' + dim + '\n');
+    var [nic,nicC] = handleCalculation(dimC,.05);
+    console.log('Nickels: ' + nic + '\n');
+    var [pen,] = handleCalculation(nicC,.01);
+    console.log('Pennies: ' + pen + '\n'); */
+    result = [dol,qrt,dim,nic,pen];
+    return result;
+}
+function handleCalculation(num,div) {
+   /* console.log(num);
+    num = parseInt(String(num * 100));
+    div = parseInt(String(div * 100));
+    var rem = num / 100 - parseInt(String(num / div));
+    console.log(rem);
+    var result = [parseInt(String(num / div)),parseInt(String(rem)) / 100];
+    console.log('Num: ' + (num / 100) + ' Div: ' + (div / 100) + ' Rem: ' + (rem / 100) + ' Result: ' + result[0] + '\n'); */
+    var result = 0;
+    while(num - ((result + 1) * div) >= 0) {
+        result++; 
+        // console.log(result * div + ' ' + result + ' ' + (num - (result * div)));
+    }
+    var remainder = num - (result * div);
+    // console.log('Result: ' + result + ' Remainder: ' + remainder);
+    return [result,remainder];
+}
+
+function handleClickEvent() {
+    // console.log(document.getElementById('amount-due').value);
+    // console.log(document.getElementById('amount-received').value);
+    if(parseInt(String(document.getElementById('amount-due').value * 100)) > parseInt(String(document.getElementById('amount-received').value * 100))) {
+        //console.log('The customer didn\'t give enough money!');
+    document.getElementById('error-field').textContent = 'The customer didn\'t give you enough money!';
+    document.getElementById('dollars-output').textContent = '';
+    document.getElementById('quarters-output').textContent = '';
+    document.getElementById('dimes-output').textContent = '';
+    document.getElementById('nickels-output').textContent = '';
+    document.getElementById('pennies-output').textContent = '';
+    }
+    else {
+    var result = calculateChange(parseInt(String(document.getElementById('amount-due').value * 100)),parseInt(String(document.getElementById('amount-received').value * 100)));
+    document.getElementById('error-field').textContent = '';
+    document.getElementById('dollars-output').textContent = 'Dollars: ' + result[0];
+    document.getElementById('quarters-output').textContent = 'Quarters: ' + result[1];
+    document.getElementById('dimes-output').textContent = 'Dimes: ' + result[2];
+    document.getElementById('nickels-output').textContent = 'Nickels: ' + result[3];
+    document.getElementById('pennies-output').textContent = 'Pennies: ' + result[4];
+    }
+}
+
+document.getElementById('calculate-change').onclick = function(){
+    // console.log('Click!');
+    handleClickEvent();
+};
+
+    //console.log('Result: ' + calculateChange(1021,2000) + '\n');

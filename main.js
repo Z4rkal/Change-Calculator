820e53d960e36c2e5f3c3eb8cc857f7e3fbfc116
Michael Fischer <michael@Jonathans-MacBook-Pro.local>
2019-05-31 15:54:09 -0700

    Finished up CSS and added functionality for different sizes of dollar bills

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ function calculateChange(amtDue,amtGiven) {
 */  var result = []; //[dollars,quarters,dimes,nickels,pennies]
     changeDue = amtGiven - amtDue;
     // console.log('Change due: ' + changeDue);
-    // var [twdol,twdolC] = handleCalculation(changeDue,2000);
-    // var [tdol,tdolC] = handleCalculation(twdolC,1000);
-    // var [fdol,fdolC] = handleCalculation(tdolC,500);
-    // var [ddol,ddolC] = handleCalcualtion(fdolC,200);
-    var [dol,dolC] = handleCalculation(changeDue,100);
+    var [twdol,twdolC] = handleCalculation(changeDue,2000);
+    var [tdol,tdolC] = handleCalculation(twdolC,1000);
+    var [fdol,fdolC] = handleCalculation(tdolC,500);
+    var [ddol,ddolC] = handleCalculation(fdolC,200);
+    var [dol,dolC] = handleCalculation(ddolC,100);
     // console.log(dol + ' ' + dolC);
     var [qrt,qrtC] = handleCalculation(dolC,25);
     var [dim,dimC] = handleCalculation(qrtC,10);
@@ -27,7 +27,7 @@ function calculateChange(amtDue,amtGiven) {
     console.log('Nickels: ' + nic + '\n');
     var [pen,] = handleCalculation(nicC,.01);
     console.log('Pennies: ' + pen + '\n'); */
-    result = [dol,qrt,dim,nic,pen];
+    result = [twdol,tdol,fdol,ddol,dol,qrt,dim,nic,pen];
     return result;
 }
 function handleCalculation(num,div) {
@@ -55,6 +55,12 @@ function handleClickEvent() {
         //console.log('The customer didn\'t give enough money!');
     document.getElementById('error-field').textContent = 'The customer didn\'t give you enough money!';
     document.getElementById('dollars-output').textContent = '0';
+    document.getElementById('twdol-output').textContent = '0';
+    document.getElementById('tdol-output').textContent = '0';
+    document.getElementById('fdol-output').textContent = '0';
+    document.getElementById('ddol-output').textContent = '0';
+    document.getElementById('dol-output').textContent = '0';
+    document.getElementById('dollars-output').textContent = '0';
     document.getElementById('quarters-output').textContent = '0';
     document.getElementById('dimes-output').textContent = '0';
     document.getElementById('nickels-output').textContent = '0';
@@ -63,11 +69,16 @@ function handleClickEvent() {
     else {
     var result = calculateChange(parseInt(String(document.getElementById('amount-due').value * 100)),parseInt(String(document.getElementById('amount-received').value * 100)));
     document.getElementById('error-field').textContent = '';
-    document.getElementById('dollars-output').textContent = result[0];
-    document.getElementById('quarters-output').textContent = result[1];
-    document.getElementById('dimes-output').textContent = result[2];
-    document.getElementById('nickels-output').textContent = result[3];
-    document.getElementById('pennies-output').textContent = result[4];
+    document.getElementById('dollars-output').textContent = (result[0] * 20 + result[1] * 10 + result[2] * 5 + result[3] * 2 + result[4]);
+    document.getElementById('twdol-output').textContent = result[0];
+    document.getElementById('tdol-output').textContent = result[1];
+    document.getElementById('fdol-output').textContent = result[2];
+    document.getElementById('ddol-output').textContent = result[3];
+    document.getElementById('dol-output').textContent = result[4];
+    document.getElementById('quarters-output').textContent = result[5];
+    document.getElementById('dimes-output').textContent = result[6];
+    document.getElementById('nickels-output').textContent = result[7];
+    document.getElementById('pennies-output').textContent = result[8];
     }
 }
 
